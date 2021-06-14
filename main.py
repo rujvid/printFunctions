37e52a974f5aafa625a232d8358dbc38b2396d78
rujvid <Rujvi.doppalapudi@gmail.com>
2021-06-14 15:30:21 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+name = input('Type your name: ')
+school = input("What's your school name? ")
+grade = int(input('what grade are you in? '))
+
+print(name + ' goes to ' + school + ' to study grade # ' + str(grade))
